e2671ab0aa6e5edf16df095f1355043cf80fdb93
Luis Fernando Saquetim Vendrame <luis.vendrame@compline.com.br>
2018-01-04 19:25:57 -0200

    Exercise 7 - Communication

---
+++ Exercise-7/src/components/Server/ServerItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <li
+        class="list-group-item">
+        <a href="javascript:void(0)">Server #{{ id }}</a>
+    </li>
+</template>
+
+<script>
+    export default {
+        props: ['id']
+    }
+</script>
+
+<style>
+
+</style>
---
+++ Exercise-7/src/components/Server/Servers.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">            
+            <server-item v-for="server in servers" :key="server.id"
+             :id="server.id"></server-item>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import ServerItem from './ServerItem.vue';
+
+    export default {
+        data: function() {
+            return {
+                servers: [
+                    {id: 1, status: 'Normal'},
+                    {id: 2, status: 'Critical'},
+                    {id: 3, status: 'Unknow'},
+                    {id: 4, status: 'Normal'},
+                    {id: 5, status: 'Normal'}
+                ]
+            }
+        },
+        components: {
+            'server-item': ServerItem
+        }
+    };
+</script>
+<style>
+
+</style>
