e9ada16d9ba506a43d5c39ead3f836bcc8f253b9
Luis Fernando Saquetim Vendrame <luis.vendrame@compline.com.br>
2018-01-19 18:31:12 -0200

    Section 11 - 2

--- Section-11 - Component/src/App.vue
+++ Section-11 - Component/src/App.vue
@@ -9,7 +9,8 @@
                         <label for="email">Mail</label>
                         <input
                                 type="text" id="email" class="form-control"
-                                v-model="userData.email">
+                                :value="userData.email"
+                                @input="userData.email = $event.target.value">
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
