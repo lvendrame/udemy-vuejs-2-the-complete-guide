1318603c8eee481306fb42d8fb8332627f078141
Luis Fernando Saquetim Vendrame <lvendrame@gmail.com>
2018-01-22 18:37:45 -0200

    Section 11 - Exercise [finished]

--- Section-11-Exercise/src/App.vue
+++ Section-11-Exercise/src/App.vue
@@ -1,37 +1,37 @@
 <template>
     <div class="container">
-        <form>
+        <form v-if="!isSubmitted">
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <!-- Exercise 1 -->
-                    <!-- Create a Signup Form where you retrieve the following Information -->
-                    <!-- Full Name (First Name + Last Name) -->
-                    <!-- Mail -->
-                    <!-- Password -->
-                    <!-- Store Data? Yes/No -->
-
-                    <!-- Exercise 2 -->
-                    <!-- Only display the Form if it has NOT been submitted -->
-                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
-
-                    <!-- Exercise 3 -->
-                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
-                    <!-- which still holds the First Name and Last Name Input Field -->
+                    <full-name v-model="user.name"></full-name>                        
+                    <div class="form-group">
+                        <label for="email">E-Mail</label>
+                        <input type="email" name="email" id="email" class="form-control" v-model="user.mail">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" class="form-control" v-model="user.password">
+                    </div>
+                    <div class="form-group">
+                        <input type="checkbox" name="store" id="store" class="form-check-input" v-model="storeInDataBase">
+                        <label for="store" class="form-check-label">Store in Database?</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary" @click.prevent="isSubmitted=true">Submit</button>
                 </div>
             </div>
+            <hr>
         </form>
-        <hr>
-        <div class="row">
+        <div class="row" v-else>
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{user.name}}</p>
+                        <p>Mail: {{user.mail}}</p>
+                        <p>Password: {{user.password}}</p>
+                        <p>Store in Database?: {{storeInDataBase}}</p>
                     </div>
                 </div>
             </div>
@@ -40,7 +40,23 @@
 </template>
 
 <script>
+    import FullName from './components/FullName.vue'
+
     export default {
+        data(){
+            return {
+                user:{
+                    name: '',
+                    mail: '',
+                    password: '',
+                },
+                storeInDataBase: false,
+                isSubmitted: false
+            };
+        },
+        components:{
+            fullName: FullName
+        }
     }
 </script>
 
---
+++ Section-11-Exercise/src/components/FullName.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="form-group">
+            <label for="firstname">First Name</label>
+            <input type="text" name="firstname" id="firstname" class="form-control input-sm" v-model="firstName" @input="sendName()">            
+        </div>
+        <div class="form-group">
+            <label for="lastname">Last Name</label>
+            <input type="text" name="lastname" id="lastname" class="form-control input-sm" v-model="lastName" @input="sendName()">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['value'],
+    watch: { 
+        value(newVal, oldVal) {
+            let names = newVal.split(' ');
+            this.firstName = names[0];
+            this.lastName = names[1];
+        }
+    },
+    data(){
+        return {
+            firstName: '',
+            lastName: ''
+        }
+    },
+    methods: {
+        sendName(){
+            this.$emit('input', this.firstName + ' ' + this.lastName);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
