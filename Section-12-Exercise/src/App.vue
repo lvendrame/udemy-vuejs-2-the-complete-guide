66192277106da5f65d88502d8a183c0a0a38c3ed
Luis Fernando Saquetim Vendrame <lvendrame@gmail.com>
2018-01-25 19:18:49 -0200

    Section 12 - Exercise

--- Section-12-Exercise/src/App.vue
+++ Section-12-Exercise/src/App.vue
@@ -3,16 +3,27 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Directives Exercise</h1>
-                <!-- Exercise -->
-                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-
+                <a href="#" v-myon:click="showMessage">Click here</a>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        methods: {
+            showMessage(){
+                alert('Hello!');
+            }
+        },
+        directives:{
+            "myon":{                
+                bind(el, binding, vnode){
+                    el.addEventListener(binding.arg, binding.value);
+                }
+            }
+        }
+    }
 </script>
 
 <style>
