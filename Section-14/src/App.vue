a650cc3ee9a6e9744ed75ffade84c15e1fd6c910
Luis Fernando Saquetim Vendrame <lvendrame@gmail.com>
2018-01-31 19:02:38 -0200

    Section 14

--- Section-14/src/App.vue
+++ Section-14/src/App.vue
@@ -3,10 +3,29 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
-                <button class="btn btn-primary" @click="show = !show">Show Alert</button>
-                <br><br>
-                <transition name="fade">
+                <hr>
+                <select v-model="alertAnimation" class="form-control">
+                    <option value="fade">Fade</option>
+                    <option value="slide">Slide</option>
+                </select>
+                <br>
+                <button class="btn btn-primary" @click="show = !show">{{ show? 'Hide' : 'Show'}} Alert</button>
+                <br>
+                <br>
+                <transition :name="alertAnimation">
+                    <div class="alert alert-info" v-show="show">This is some info</div>
+                </transition>
+                <transition name="slide" type="animation" appear>
                     <div class="alert alert-info" v-if="show">This is some info</div>
+                </transition>
+                <transition
+                    enter-active-class="animated bounce"
+                    leave-active-class="animated shake">
+                    <div class="alert alert-info" v-if="show">This is some info</div>
+                </transition>
+                <transition :name="alertAnimation" mode="out-in">
+                    <div class="alert alert-info" v-if="show" key="info">This is some info</div>
+                    <div class="alert alert-warning" v-else key="warning">This is some warning</div>
                 </transition>
             </div>
         </div>
@@ -17,7 +36,8 @@
     export default {
         data() {
             return {
-                show: false
+                show: true,
+                alertAnimation: 'fade'
             }
         }
     }
@@ -35,5 +55,40 @@
     .fade-leave-active{
         transition: opacity 1s;
         opacity: 0;
+    }
+
+    
+    .slide-enter{
+        transform: translateY(20px);
+        opacity: 0;
+    }
+    .slide-enter-active{
+        animation: slide-in 1s ease-out forwards;
+        transition: opacity .5s;
+    }
+    .slide-leave{
+    }
+    .slide-leave-active{
+        animation: slide-out 1s ease-out forwards;
+        transition: opacity 1s;
+        opacity: 0;
+    }
+
+    @keyframes slide-in {
+        from{
+            transform: translateY(20px);
+        }
+        to{
+            transform: translateY(0);
+        }
+    }
+
+    @keyframes slide-out {
+        from{
+            transform: translateY(0);
+        }
+        to{
+            transform: translateY(20px);
+        }
     }
 </style>
