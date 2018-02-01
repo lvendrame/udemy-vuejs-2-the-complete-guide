6b9cb3a49f7828169c135cf312bd3cc0e98a6b5c
lvendrame <lvendrame@gmail.com>
2018-02-01 09:41:24 -0200

    section 14

--- Section-14/src/App.vue
+++ Section-14/src/App.vue
@@ -27,6 +27,23 @@
                     <div class="alert alert-info" v-if="show" key="info">This is some info</div>
                     <div class="alert alert-warning" v-else key="warning">This is some warning</div>
                 </transition>
+                <hr>
+                <button class="btn btn-primary" @click="load=!load">Load / Remove Element</button>
+                <br><br>
+                <transition
+                    @before-enter="beforeEnter"
+                    @enter="enter"
+                    @after-enter="afterEnter"
+                    @enter-cancelled="enterCancelled"
+
+                    @before-leave="beforeLeave"
+                    @leave="leave"
+                    @after-leave="afterLeave"
+                    @leave-cancelled="leaveCancelled"
+                    :css="false"
+                    >
+                    <div style="width:100px;height:100px;background-color:lightgreen;" v-if="load"></div>
+                </transition>
             </div>
         </div>
     </div>
@@ -36,8 +53,37 @@
     export default {
         data() {
             return {
-                show: true,
+                show: false,
+                load: true,
                 alertAnimation: 'fade'
+            }
+        },
+        methods:{
+            beforeEnter(el){
+                console.log('beforeEnter');
+            },
+            enter(el, done){
+                console.log('enter');
+                done();
+            },
+            afterEnter(el){
+                console.log('afterEnter');
+            },
+            enterCancelled(el){
+                console.log('enterCancelled');
+            },
+            beforeLeave(el){
+                console.log('beforeLeave');
+            },
+            leave(el, done){
+                console.log('leave');
+                done();
+            },
+            afterLeave(el){
+                console.log('afterLeave');
+            },
+            leaveCancelled(el){
+                console.log('leaveCancelled');
             }
         }
     }
