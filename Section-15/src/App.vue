2504a3631b4aa74ecf691ab22b54b4a4628c180a
lvendrame <lvendrame@gmail.com>
2018-02-05 09:39:42 -0200

    Section 15

---
+++ Section-15/src/App.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Http</h1>
+                <div class="form-group">
+                    <label for="">Username</label>
+                    <input type="text" class="form-control" v-model="user.username">
+                </div>
+                <div class="form-group">
+                    <label for="">Mail</label>
+                    <input type="text" class="form-control" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
+                <hr>
+                <button class="btn btn-primary" @click="fetchData">Get Data</button>
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="user in users" :key="user.id">
+                        {{user.username}} - {{user.email}}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                user: {
+                    username:'',
+                    email:''
+                },
+                users: [],
+                resource: {}
+            };
+        },
+        methods: {
+            submit(){
+                // this.$http.post('data.json', this.user)
+                //     .then(response => {
+                //         console.log(response);
+                //     })
+                //     .catch(error => {
+                //         console.log(error);
+                //     });
+                //this.resource.save({}, this.user);
+                this.resource.saveAlt(this.user);
+            },
+            fetchData(){
+                this.$http.get('data.json')
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data => {
+                        const resultArray = [];
+                        for (const key in data) {
+                            if (data.hasOwnProperty(key)) {
+                                const item = data[key];
+                                item.id = key;
+                                resultArray.push(item);                                
+                            }
+                        }
+                        this.users = resultArray;
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            }
+        },
+        created(){
+            const customActions = {
+                saveAlt: {method: 'POST', url: 'alternative.json'}
+            };
+            this.resource = this.$resource('data.json', {}, customActions);
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ Section-15/src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+import App from './App.vue'
+
+Vue.use(VueResource);
+
+Vue.http.options.root = "https://vuejs-http-d5590.firebaseio.com/";
+Vue.http.interceptors.push((request, next) => {
+    console.log(request);
+    if (request.method === 'POST') {
+        request.method = 'PUT';
+    }
+    next(response => {
+        response.json = () => {
+            return { messages: response.body };
+        };
+    });
+});
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+})
