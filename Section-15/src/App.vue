bc2b16bf7de811cdfc9ed9df1935536bee498be0
Luis Fernando Saquetim Vendrame <lvendrame@gmail.com>
2018-02-05 18:43:46 -0200

    Section 15; Section 16

--- Section-15/src/App.vue
+++ Section-15/src/App.vue
@@ -13,6 +13,8 @@
                 </div>
                 <button class="btn btn-primary" @click="submit">Submit</button>
                 <hr>
+                <input type="text" class="form-control" v-model="node">
+                <br><br>
                 <button class="btn btn-primary" @click="fetchData">Get Data</button>
                 <ul class="list-group">
                     <li class="list-group-item" v-for="user in users" :key="user.id">
@@ -33,7 +35,8 @@
                     email:''
                 },
                 users: [],
-                resource: {}
+                resource: {},
+                node: 'data'
             };
         },
         methods: {
@@ -49,7 +52,8 @@
                 this.resource.saveAlt(this.user);
             },
             fetchData(){
-                this.$http.get('data.json')
+                // this.$http.get('data.json')
+                this.resource.getData({node:this.node})
                     .then(response => {
                         return response.json();
                     })
@@ -71,9 +75,10 @@
         },
         created(){
             const customActions = {
-                saveAlt: {method: 'POST', url: 'alternative.json'}
+                saveAlt: {method: 'POST', url: 'alternative.json'},
+                getData: {method: 'GET'}
             };
-            this.resource = this.$resource('data.json', {}, customActions);
+            this.resource = this.$resource('{node}.json', {}, customActions);
         }
     }
 </script>
