57235170433e3ed14aa7524d3d728e7a58fa9fb3
Luis Fernando Saquetim Vendrame <lvendrame@gmail.com>
2018-02-08 19:27:37 -0200

    Section 16

--- Section-16/src/components/user/User.vue
+++ Section-16/src/components/user/User.vue
@@ -1,4 +1,23 @@
 <template>
-    <h1>The User Page</h1>
-
+    <div>
+        <h1>The User Page</h1>
+        <hr>
+        <button class="btn btn-primary" @click="navigateToHome">Go To Home</button>
+        <hr>
+        <router-view></router-view>
+    </div>
 </template>
+
+<script>
+    export default {
+        methods:{
+            navigateToHome(){
+                this.$router.push({ name: 'home' });
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- Section-16/src/routes.js
+++ Section-16/src/routes.js
@@ -1,7 +1,18 @@
 import User from './components/user/User.vue';
+import UserDetail from './components/user/UserDetail.vue';
+import UserEdit from './components/user/UserEdit.vue';
+import UserStart from './components/user/UserStart.vue';
 import Home from './components/Home.vue';
 
 export const routes = [
-    { path: '', component: Home },
-    { path: '/user', component: User }
+    { path: '', component: Home, name: 'home' },
+    {
+        path: '/user',
+        component: User,
+        children: [
+            { path: '', component: UserStart },
+            { path: ':id', component: UserDetail },
+            { path: ':id/edit', component: UserEdit, name: 'userEdit' },
+        ]
+    }
 ];
